0bdc58464606ced7e6c0f2d5b4a683dd23aad705
Dan Mercer <danielmercer1@gmail.com>
2017-10-17 20:56:32 -0600

    Added wizard directive

--- madlib.js
+++ madlib.js
@@ -1,30 +1,34 @@
 angular.module('app', [])
   .controller('mainCtrl', mainCtrl)
-  .directive('noun', nounDirective);
+  .directive('word', directive);
 
 function mainCtrl ($scope) {
-  $scope.words = ["Noun","Preposition","Verb","Noun"];
+  $scope.words = [];
   $scope.values = [];
 
   // Click listener for testing:
   $scope.onInputChange = function() {
-
+    console.log($scope);
   }
 }
 
-function nounDirective () {
+var counter = 0;
+function directive () {
   return {
     restrict: 'E',
     replace: 'true',
-    template: function() {
-      console.log("template", arguments);
-      return "<p>Bagels are good</p>"
-    }, /* [3] */
+    template: template,
     link: link
   };
 
-  function link (scope) { /* [4] */
-    console.log("link", arguments);
+  function template($el, $attrs) {
+    const idx = counter++;
+    const type = $el.text();
+    const init = 'type="'+type+'"';
+    return "<span ng-init='"+init+"'>{{ values["+idx+"] || '__________' }}</span>"
   }
 
+  function link ($scope, $attrs) {
+    $scope.words.push($scope.type);
+  }
 }
