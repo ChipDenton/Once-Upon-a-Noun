a8d5e2ff81c48dbec915cfe9ba8c9bebea1ab6a7
Chip Denton <chipstopherscott@gmail.com>
2017-10-18 01:29:00 +0000

    Placeholder text

--- madlib.js
+++ madlib.js
@@ -1,1 +1,37 @@
- 
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', madlibDirective);
+
+function mainCtrl ($scope) {
+  
+
+  };
+}
+
+function madlibDirective () {
+  return {
+    scope: {
+      
+    },
+    restrict: 'E',
+    replace: 'true',
+    template: (
+      '<form class="Avatar">' +
+        '<img ng-src="{{user.avatarUrl}}" />' +
+        '<h4>{{user.name}}</h4>' +
+	'<h4>{{user.email}}</h4>' +
+      '</div>'
+    ), /* [3] */
+    link: link
+  };
+  
+  function link (scope) { /* [4] */
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
+    }
+    if(!scope.user.name){scope.user.name="No Name Provided";}
+    if(!scope.user.email){scope.user.email="No Email Provided";}
+  }
+
+
+}
